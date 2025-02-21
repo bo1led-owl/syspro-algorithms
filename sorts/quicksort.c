55016d09c2c442fe04c1f16a8f8f22322775689b
Ilya Sovtsov <ilyasovty@gmail.com>
2025-02-21 18:35:15 +0700

    lomuto

--- sorts/quicksort.c
+++ sorts/quicksort.c
@@ -1,8 +1,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-// lomuto:
+// lomuto: https://leetcode.com/problems/sort-an-array/submissions/1550672550/
 // hoare: https://leetcode.com/problems/sort-an-array/submissions/1545275975/
+
+#define LOMUTO
 
 static void swap(int* a, size_t i, size_t j) {
     int tmp = a[i];
@@ -10,19 +12,32 @@ static void swap(int* a, size_t i, size_t j) {
     a[j] = tmp;
 }
 
-static size_t lomutoPartition(int* a, size_t l, size_t r) {
-    size_t pivot_index = l + rand() % (r - l + 1);
+typedef struct {
+    size_t l, h;
+} PartitionResult;
+
+static PartitionResult lomutoPartition(int* a, size_t low, size_t high) {
+    size_t pivot_index = low + rand() % (high - low + 1);
     int pivot = a[pivot_index];
 
-    size_t i = l - 1;
-    for (size_t j = l; j <= r; ++j) {
-        if (a[j] <= pivot) {
-            i += 1;
-            if (i != j) swap(a, i, j);
+    size_t l, h;
+    l = h = low;
+
+    for (size_t c = low; c <= high; ++c) {
+        if (a[c] < pivot) {
+            int tmp = a[c];
+            a[c] = a[h];
+            a[h] = a[l];
+            a[l] = tmp;
+            ++l;
+            ++h;
+        } else if (a[c] == pivot) {
+            swap(a, h, c);
+            ++h;
         }
     }
 
-    return i + 1;
+    return (PartitionResult){l, h};
 }
 
 static size_t hoarePartition(int* a, size_t l, size_t r) {
@@ -54,10 +69,20 @@ static void quickSort(int* a, size_t l, size_t r) {
         return;
     }
 
-    size_t pivot = lomutoPartition(a, l, r);
+#if defined(HOARE)
+    size_t pivot = hoarePartition(a, l, r);
 
-    quickSort(a, l, pivot - 1);
-    quickSort(a, pivot, r);
+    quickSort(a, l, pivot);
+    quickSort(a, pivot + 1, r);
+#elif defined(LOMUTO)
+    PartitionResult pivots = lomutoPartition(a, l, r);
+
+    if (pivots.l > 0)
+        quickSort(a, l, pivots.l - 1);
+    quickSort(a, pivots.h, r);
+#else
+#error No partition algorithm chosen
+#endif
 }
 
 int* sortArray(int* nums, int numsSize, int* returnSize) {
