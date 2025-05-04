7ce5e4f672d4acf8770c1ef618073e107071320d
Ilya Sovtsov <ilyasovty@gmail.com>
2025-05-04 14:46:32 +0700

    generalize kosaraju

--- graphs/scc/kosaraju.scala
+++ graphs/scc/kosaraju.scala
@@ -2,18 +2,17 @@ import scala.io.Source
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.HashSet
 
-type Graph = HashMap[String, List[String]]
+type Graph[T] = HashMap[T, List[T]]
 
-object Graph:
-  def fromLines(lines: IterableOnce[String]): Graph =
-    def parseFunction(line: String) =
-      val pattern = "\\s*(\\w+)\\s*:\\s*(.*)".r
-      line match
-        case pattern(name, callees) => (name, ",\\s*".r.split(callees).toList)
-    HashMap.from(lines.iterator.map(parseFunction))
+def parseFunctions(lines: IterableOnce[String]): Graph[String] =
+  def parseFunction(line: String) =
+    val pattern = "\\s*(\\w+)\\s*:\\s*(.*)".r
+    line match
+      case pattern(name, callees) => (name, ",\\s*".r.split(callees).toList)
+  HashMap.from(lines.iterator.map(parseFunction))
 
 @main def main() =
-  val fns = Graph.fromLines(Source.stdin.getLines())
+  val fns = parseFunctions(Source.stdin.getLines())
   println("SCCs:")
   for scc <- Kosaraju(fns).run do println(s"  ${scc.mkString(", ")}")
 
@@ -21,32 +20,30 @@ object Graph:
   for fn <- fns.keys do DFS(fns, _ => (), _ => (), recursiveFunctions += _).run(fn)
   println(s"recursive functions: ${recursiveFunctions.mkString(", ")}")
 
-class Kosaraju(graph: Graph, revgraph: Graph):
-  private var time: HashMap[String, Int] = HashMap()
-  private var maxTime: Int               = 0
-  def this(g: Graph) = this(g, reverseGraph(g))
-  def run: List[List[String]] =
-    var timeDfs = DFS(revgraph, _ => (), node => { time += (node, maxTime); maxTime += 1 })
+class Kosaraju[T](graph: Graph[T], revgraph: Graph[T]):
+  private var time: HashMap[T, Int] = HashMap()
+  private var maxTime: Int          = 0
+  def this(g: Graph[T]) = this(g, reverseGraph(g))
+  def run: List[List[T]] =
+    var timeDfs = DFS[T](revgraph, _ => (), node => { time += (node, maxTime); maxTime += 1 })
     for v <- revgraph.keys do timeDfs.run(v)
-    var sccs: List[List[String]] = Nil
-    var curScc: List[String]     = Nil
-    var sccDfs                   = DFS(graph, node => curScc :+= node)
+    var sccs: List[List[T]] = Nil
+    var curScc: List[T]     = Nil
+    var sccDfs              = DFS(graph, node => curScc :+= node)
     for v <- graph.keys.toList.sortBy(time(_)).reverse do
       curScc = Nil
       sccDfs.run(v)
       if !curScc.isEmpty then sccs :+= curScc
     sccs
 
-class DFS(
-    graph: Graph,
-    onEnter: String => Unit = _ => (),
-    onExit: String => Unit = _ => (),
-    onLoop: String => Unit = _ => ()
+class DFS[T](
+    graph: Graph[T],
+    onEnter: T => Unit = (_: T) => (),
+    onExit: T => Unit = (_: T) => (),
+    onLoop: T => Unit = (_: T) => ()
 ):
-  private var visited: HashSet[String]                 = HashSet()
-  private def impl(v: String, f: String => Unit): Unit = ???
-
-  def run(v: String): Unit =
+  private var visited: HashSet[T] = HashSet()
+  def run(v: T): Unit =
     if !graph.contains(v) then return
     if visited.contains(v) then
       onLoop(v)
@@ -56,8 +53,8 @@ class DFS(
     for u <- graph(v) do run(u)
     onExit(v)
 
-def reverseGraph(g: Graph): Graph =
-  var newGraph: Graph = HashMap()
+def reverseGraph[T](g: Graph[T]): Graph[T] =
+  var newGraph: Graph[T] = HashMap()
   for
     (v, edges) <- g
     u          <- edges
